1ff83d848ef17580fe4e8fd0638bc2c6d5f14238
Normunds Stivrins <normunds.stivrins@protonmail.com>
2021-12-18 11:09:23 +0200

    addedSomeTestAssert

---
+++ src/test/java/lv/sda/books/ApplicationTest.java
@@ -0,0 +1,18 @@
+package lv.sda.books;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+   @Test
+   public void sampleTest(){
+      assertEquals(1,1);
+   }
+
+
+
+}
