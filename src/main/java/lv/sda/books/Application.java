8a63f2f3b674236db3585b70d75d2115962d9212
Diana Gintere <diana.gintere@gmail.com>
2021-12-18 15:47:06 +0200

    updated Application class

--- src/main/java/lv/sda/books/Application.java
+++ src/main/java/lv/sda/books/Application.java
@@ -1,9 +1,60 @@
 package lv.sda.books;
 
+import java.util.Scanner;
+
 public class Application {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        while (true) {
+            System.out.println("Menu");
+            System.out.println("1. Search for a book");
+            System.out.println("2. Add a book");
+            System.out.println("3. Remove a book");
+            System.out.println("4. Get book info");
+            System.out.println("5. List available books");
+            System.out.println("To quit press: q");
+
+            String input = scanner.nextLine();
+
+            if ("q".equalsIgnoreCase(input)) {
+                System.out.println("Quitting application.");
+                break;
+            }
+
+            switch (input) {
+                case "1":
+                    System.out.println("Searching for a book");
+                    break;
+                case "2":
+                    System.out.println("Adding a book");
+                    break;
+                case "3":
+                    System.out.println("Removing a book");
+                    System.out.println("Provide isbm of the book to remove:");
+                    scanner.nextLine();
+                    break;
+                case "4":
+                    System.out.println("Getting book info");
+                    break;
+                case "5":
+                    System.out.println("Listing available books:");
+                    break;
+                default:
+                    System.out.println("Wrong input, please try again.");
+            }
+
+            System.out.println();
+        }
     }
 
 
