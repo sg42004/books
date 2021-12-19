9a33290cf1f11532a99776d7d92e51a358c39903
Normunds Stivrins <normunds.stivrins@protonmail.com>
2021-12-19 08:52:22 +0200

    minor change in Application class (first try/catch)

--- src/main/java/lv/sda/books/Application.java
+++ src/main/java/lv/sda/books/Application.java
@@ -134,26 +134,7 @@ public class Application {
 
 
     /*
-    viens no paņēmieniem, ielasīšanai no faila
-try {
-    Path path = Paths.get("src/main/resources/books.txt");
-    List<Book> books = Files.lines(path)
-            .map(line -> {
-                List<String> fields = Arrays.stream(line.split(";")).collect(toList());
-                return new Book(
-                        fields.get(0),
-                        fields.get(1),
-                        fields.get(2),
-                        fields.get(3),
-                        fields.get(4),
-                        Integer.parseInt(fields.get(5)),
-                        LocalDate.of(Integer.parseInt(fields.get(6)), 1, 1)
-                );
-            })
-            .collect(Collectors.toList());
-    books.forEach(System.out::println);
-} catch (Exception e) {
-    e.printStackTrace();
+    viens
 }*/
 
 
