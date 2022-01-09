f4c3cb6f3b15d190e6029782a80a84f011db39d2
Normunds Stivrins <normunds.stivrins@protonmail.com>
2022-01-09 09:52:47 +0200

    copy/past code J/U

--- src/main/java/lv/sda/books/Bookstore.java
+++ src/main/java/lv/sda/books/Bookstore.java
@@ -1,14 +1,12 @@
 package lv.sda.books;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
@@ -17,117 +15,132 @@ import static java.util.stream.Collectors.toList;
 
 public class Bookstore {
 
-    List<Book> books;
-    public Bookstore(){
+    // Upload file to array list
+    public List<Book> booksList = new ArrayList<>();
+
+    public Bookstore() {
+        readBooksFromFile("src\\main\\resources\\BookList.csv");
+    }
+
+    public Bookstore(String file) {
+        readBooksFromFile(file);
+    }
+
+    private void readBooksFromFile(String file) {
         try {
-            Path path = Paths.get("src/main/resources/books.txt");
-            List<Book> books = Files.lines(path)
-                    .map(line -> {
-                        List<String> fields = Arrays.stream(line.split(";")).collect(toList());
-                        return new Book(
-                                fields.get(0),
-                                fields.get(1),
-                                fields.get(2),
-                                fields.get(3),
-                                Integer.parseInt(fields.get(4)),
-                                Integer.parseInt(fields.get(5)),
-                                fields.get(6)
-                        );
-                    })
-                    .collect(Collectors.toList());
-            books.addAll(books);
-        } catch (Exception e) {
+            List<String> lines = Files.readAllLines(Paths.get(file));
+
+            List<Book> books = lines.stream().map(line -> {
+                List<String> fields = Arrays.asList(line.split(";"));
+                return new Book(
+                        fields.get(0),
+                        fields.get(1),
+                        fields.get(2),
+                        fields.get(3),
+                        fields.get(4),
+                        Integer.parseInt(fields.get(5)),
+                        Integer.parseInt(fields.get(6))
+                );
+            }).collect(Collectors.toList());
+            booksList.addAll(books);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void addBook() {
-        System.out.println("Input the following information");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("The isbn:");
-        String isbn = scanner.nextLine();
-        System.out.println("The title:");
-        String title = scanner.nextLine();
-        System.out.println("The description:");
-        String description = scanner.nextLine();
-        System.out.println("The author:");
-        String author = scanner.nextLine();
-        System.out.println("The page count:");
-        String pages = scanner.nextLine();
-        System.out.println("The publishing year:");
-        String year = scanner.nextLine();
-        System.out.println("The publisher:");
-        String publisher = scanner.nextLine();
-
-        System.out.println("Adding the book");
-
-        try {
-            FileWriter fw = new FileWriter("src/main/resources/books.txt", true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.newLine();
-            bw.write(isbn + ", " + title + ", " + description + ", " + author + ", " + pages + ", " + year + ", " + publisher);
-            bw.close();
-            System.out.println("Book added successfully.");
-        } catch (Exception e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+    // Get book list
+    public void printList(){
+        System.out.println("Book list:");
+        for (Book i : booksList){
+            System.out.println(i);
         }
     }
 
-    public void removeBook(String isbn){
-
-    }
-
-
-
-
-    public static void getInfo() throws FileNotFoundException {
-        System.out.println("Please insert ISBN of the book you are looking for: ");
-        Scanner scanner = new Scanner(System.in);
-        String isbn = scanner.nextLine();
-        while (scanner.hasNext()) {
-            String line = scanner.nextLine().toLowerCase(Locale.ROOT);
-            if (line.contains(isbn)) {
-                System.out.println(line);
+    // Add book
+    public void addBook(Book i) {
+        boolean isDuplicate= false;
+        for(int j = 0; j < booksList.size(); j++){
+            if(!(i.getIsbn().equals(booksList.get(j).getIsbn()))){
+            } else {
+                isDuplicate = true;
+                System.out.println("This book already exists!");
             }
         }
-    }
-    //print options what to do further with the book (buy yes/no)
-        //System.out.println("Would you like to buy this book (yes/no): ");
-
-        //search for book by isbn (String type)
-        //if correct then print full info on book
-
-
-
-
-        //if there is no such book -> print an options for further actions (type again/return to menu/q)
-       // System.out.println("Unfortunately, at the moment we do not have this book in the store. ");
-
-
-
-    public List<Book> searchBook(String query){
-        System.out.println("Type name of the book you are looking for: ");
-
-        return Collections.emptyList();
-    }
-
-    public static List<Book> allBooks(){
-        System.out.println("Listing available books:");
-        try {
-            File myObj = new File("src/main/resources/books.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+        if(!isDuplicate){
+            booksList.add(i);
+            System.out.println("Book successfully added to the database.");
         }
-        return Collections.emptyList();
     }
 
+    // Remove book
+    public void removeBook(String isbn) {
+        Book one = booksList.stream().filter
+                (y -> y.getIsbn().equals(isbn)).findFirst().orElse(null);
+        if (one != null) {
+            booksList.remove(one);
+            System.out.println("Book successfully removed from database.");
+        } else {
+            System.out.println("Record not found!");
+        }
+    }
 
+    // Find book by isbn
+    public boolean findByIsbn(String isbn) {
+        boolean x = false;
+        for (Book i : booksList) {
+            if (i.getIsbn().equals(isbn)) {
+                System.out.println(i);
+                x = true;
+            }
+        }
+        if (!x) {
+            System.out.println("Record not found!");
+        }
+        return x;
+    }
+
+    // Find book by title
+    public boolean findByTitle(String title) {
+        boolean x = false;
+        for(Book i : booksList){
+            if(i.getTitle().equalsIgnoreCase(title)){
+                System.out.println(i);
+                x = true;
+            }
+        }
+        if (!x){
+            System.out.println("Record not found!");
+        }
+        return x;
+    }
+
+    // Save to file
+    public void deleteFile()
+            throws IOException{
+        File file = new File("src\\main\\resources\\BookList.csv");
+        file.delete();
+    }
+
+    public void saveData(String fileName, String text, boolean append )
+            throws IOException{
+        File file = new File(fileName);
+        FileWriter fw = new FileWriter(file, append);
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println(text);
+        pw.close();
+    }
+
+    public void saveToFile() throws IOException {
+        for(int i = 0; i < booksList.size(); i++){
+            String dataToSave = booksList.get(i).getIsbn()
+                    +";"+booksList.get(i).getTitle()
+                    +";"+booksList.get(i).getAuthor()
+                    +";"+booksList.get(i).getPublisher()
+                    +";"+booksList.get(i).getGenre()
+                    +";"+booksList.get(i).getPages()
+                    +";"+booksList.get(i).getPublishingYear();
+
+            saveData("src\\main\\resources\\BookList.csv", dataToSave,true);
+        }
+    }
 }
--- src/test/java/lv/sda/books/ApplicationTest.java
+++ src/test/java/lv/sda/books/ApplicationTest.java
@@ -8,10 +8,43 @@ import org.junit.jupiter.api.Test;
 
 public class ApplicationTest {
 
-   @Test
-   public void sampleTest(){
-      assertEquals(1,1);
-   }
+//   @Test
+//   public void findByIsbnTest(){
+//      Bookstore bs = new Bookstore("src\\test\\resources\\TestBookList.csv");
+//      assertEquals(true, bs.findByIsbn("140449493"));
+//   }
+//
+//   @Test
+//   public void findByTitleTest(){
+//      Bookstore bs = new Bookstore("src\\test\\resources\\TestBookList.csv");
+//      assertEquals(true,bs.findByTitle("The Nicomachean Ethics"));
+//   }
+//
+//   @Test
+//   public void addBookTest(){
+//      Bookstore bs = new Bookstore("src\\test\\resources\\TestBookList.csv");
+//
+//      Book book = new Book("test1","test1","test1","test1","test1",100,1000);
+//
+//      int initialSize = bs.booksList.size();
+//
+//
+//      bs.addBook(book);
+//
+//      assertEquals(initialSize+1,bs.booksList.size(),"check");
+//   }
+//
+//   @Test
+//   public void removeBookTest(){
+//      Bookstore bs = new Bookstore("src\\test\\resources\\TestBookList.csv");
+//
+//      int initSize = bs.booksList.size();
+//
+//      bs.removeBook("140449493");
+//
+//      assertEquals(initSize-1,bs.booksList.size(), "check");
+//
+//   }
 
 
 
